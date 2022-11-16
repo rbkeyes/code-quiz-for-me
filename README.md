005a1deae98f7b13bdf6b9542c81f1e0372f5bcc
Reed Keyes <rbkeyes@gmail.com>
2022-11-16 10:12:16 -0800

    added question section to index.html

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # code-quiz-for-me
 A timed, multiple-choice quiz on JavaScript fundamentals that stores high scores.
+
+# resources
+
+[Creaate a multiple choice quiz using JavaScript](https://www.codeexplained.dev/2018/10/create-multiple-choice-quiz-using-javascript.html)
---
+++ assets/js/pseudocode.js
@@ -0,0 +1,51 @@
+// Primary HTML doc
+
+// header with "check high scores" and "time remaining"
+
+// heading "Coding Quiz" center of page
+// <p> text describing quiz below
+// start button
+
+// when start button is clicked, timer begins counting down and <p> is replaced with first question
+// first question is <h2> + multiple choice buttons to select answer + "next" button (inactive until selection made)
+// button click = change button color + submit answer + deactivate buttons + display if correct or incorrect + "next" button activated
+// incorrect answer reduce countdown by 10 seconds
+// **bonus: highlight incorrect answer in red, correct answer in green
+// 
+
+// click "next", question & answers are replaced with next question (same format)
+
+// final question: click "next" to enter initials in score log
+// or:
+// timer reaches 0
+
+// <h2> with done message
+// <p> message to enter name/initials to save score
+// form to enter initials
+// "save my score" button and "cancel" button below
+
+
+// questions:
+// _______ is the process of finding errors and fixing them within a program.
+// A. Compiling
+// B. Executing
+// C. Debugging
+// D. Scanning
+
+// Which of the following variable types can hold a value of either true or false?
+// Boolean
+// string
+// number
+// array
+
+// A loop that never ends is referred to as a(n)_________.
+// While loop
+// Infinite loop
+// Recursive loop
+// for loop
+
+// What is the name of the operation that joins two strings together?
+// function
+// push
+// join
+// concatenation
