23d724872f633dbb137ac76bc39c7c1d66adcb80
Reed Keyes <rbkeyes@gmail.com>
2022-11-20 15:31:11 -0800

    first question displays, countdown starts

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,96 +2,78 @@ var startPage = document.getElementById("start-page");
 var quiz = document.getElementById("quiz");
 var quizComplete = document.getElementById("quiz-complete");
 
-// vars for question, answer choices, correct answer
-// var choices = document.getElementById("choices");
-var choice = document.querySelectorAll("choice");
+var question = document.getElementById("question");
+var selection = document.querySelectorAll("choice");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+// var answer = document.querySelector("answer");
 
-// save score to leaderboard
-function saveScore() {
-    var finalScore = document.getElementById("my-score")
-    var saveScore = document.querySelector("save-my-score")
+var quizQuestions = [
+    {
+        question: "_______ is the process of finding errors and fixing them within a program.",
+        choiceA: "compiling",
+        choiceB: "executing",
+        choiceC: "debugging",
+        choiceD: "scanning",
+    }, {
+        question: "Which of the following variable types can hold a value of either true or false?",
+        choiceA: "boolean",
+        choiceB: "string",
+        choiceC: "number",
+        choiceD: "array",
+    }, {
+        question: "A loop that never ends is referred to as a(n)_________.",
+        choiceA: "while loop",
+        choiceB: "infinite loop",
+        choiceC: "recursive loop",
+        choiceD: "for loop",
+    }, {
+        question: "What is the name of the operation that joins two strings together?",
+        choiceA: "function",
+        choiceB: "push",
+        choiceC: "join",
+        choiceD: "concatenation",
+    }
+]
 
-    
+var quizAnswers = [choiceC, choiceA, choiceB, choiceD]
+
+function displayNextQuestion() {
+    // var lastQuestion = question.length - 1;
+    let currentQuestion = 0;
+    let q = quizQuestions[currentQuestion];
+    question.textContent = q.question;
+    console.log(q.question);
+    choiceA.textContent = q.choiceA;
+    console.log(q.choiceA);
+    choiceB.textContent = q.choiceB;
+    console.log(q.choiceB);
+    choiceC.textContent = q.choiceC;
+    console.log(q.choiceC);
+    choiceD.textContent = q.choiceD;
+    console.log(q.choiceD);
+ 
+    if (currentQuestion < quiz.length) {
+        currentQuestion++
+        displayNextQuestion()
+    }
 }
 
+    // } else {
+    //     saveScore()
+    // }
 
+    // var checkAnswer = document.querySelector("checkAnswer('')");
+    // choice.addEventListener("click", checkAnswer() {
+    //     if (choice == q.answer)
+    // });
 
-    
+    // var nextBtn = document.querySelector("btn-next");
 
+    // nextBtn.addEventListener("click", displayNextQuestion)
 
-
-    var quiz = document.getElementById("quiz");
-    var question = document.getElementById("question");
-    var choiceA = document.getElementById("A");
-    var choiceB = document.getElementById("B");
-    var choiceC = document.getElementById("C");
-    var choiceD = document.getElementById("D");
-    var answer = document.querySelector("answer");
-
-    var quiz = [
-        {
-            question: "_______ is the process of finding errors and fixing them within a program.",
-            choiceA: "compiling",
-            choiceB: "executing",
-            choiceC: "debugging",
-            choiceD: "scanning",
-            answer: "C",
-        }, {
-            question: "Which of the following variable types can hold a value of either true or false?",
-            choiceA: "boolean",
-            choiceB: "string",
-            choiceC: "number",
-            choiceD: "array",
-            answer: "A",
-        }, {
-            question: "A loop that never ends is referred to as a(n)_________.",
-            choiceA: "while loop",
-            choiceB: "infinite loop",
-            choiceC: "recursive loop",
-            choiceD: "for loop",
-            answer: "B",
-        }, {
-            question: "What is the name of the operation that joins two strings together?",
-            choiceA: "function",
-            choiceB: "push",
-            choiceC: "join",
-            choiceD: "concatenation",
-            answer: "D",
-        }
-    ]
-
-  
-function displayNextQuestion() {
-
-    var lastQuestion = question.length - 1;
-    let currentQuestion = 0;
-
-        let q = quiz[currentQuestion];
-        question.textContent = q.question;
-        choiceA.textContent= q.choiceA;
-        choiceB.textContent= q.choiceB;
-        choiceC.textContent = q.choiceC;
-        choiceD.textContent = q.choiceD;
-        answer = q.answer;
-
-        if (currentQuestion < quiz.length) {
-            currentQuestion++
-            displayNextQuestion
-        } else {
-            saveScore()
-        }
-
-        var checkAnswer = document.querySelector("checkAnswer('')");
-        choice.addEventListener("click", checkAnswer() {
-            if (choice === q.answer)
-        });
-
-        
-        var nextBtn = document.querySelector("btn-next"); 
-
-        nextBtn.addEventListener("click", displayNextQuestion)
-
-    }
 
 function countdown() {
     // select timer
@@ -113,14 +95,14 @@ function countdown() {
             clearInterval(timeInterval);
 
             // Call the `saveScore()` function
-            saveScore();
+            // saveScore();
         }
     }, 1000);
 }
 
 // save your score
-function quizComplete() {
-}
+// function quizComplete() {
+// }
 
 function startQuiz() {
     if (startPage.hidden === false && quiz.hidden === true) {
@@ -134,5 +116,4 @@ function startQuiz() {
 }
 
 var startBtn = document.getElementById("start");
-
 startBtn.addEventListener("click", startQuiz);
