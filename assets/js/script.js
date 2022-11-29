1f262e58237041b42638509810ceb899efda3dd5
Reed Keyes <rbkeyes@gmail.com>
2022-11-29 13:16:27 -0800

    result displays, questions change. need quiz complete to display at end

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,5 @@
-var startPage = document.getElementById("start-page");
-var startBtn = document.getElementById("start");
-
-var quiz = document.getElementById("quiz");
-var timeLeft = 59;
-var question = document.getElementById("question");
-var choice = document.querySelectorAll(".choice");
-var choiceA = document.getElementById("A");
-var choiceB = document.getElementById("B");
-var choiceC = document.getElementById("C");
-var choiceD = document.getElementById("D");
-var choicesCorrect = 0;
-
-var quizComplete = document.getElementById("quiz-complete");
-var finalScore = document.getElementById("my-score");
-var saveBtn = document.querySelector('.save-button');
-
-var quizQuestions = [
+// set questions
+const quizQuestions = [
     {
         question: "_______ is the process of finding errors and fixing them within a program.",
         choiceA: "compiling",
@@ -47,80 +31,128 @@ var quizQuestions = [
     }
 ]
 
+// get main section elements from HTML
+const startPage = document.getElementById("start-page");
+const startBtn = document.getElementById("start");
+const quizComplete = document.getElementById("quiz-complete");
 
-function displayNextQuestion(q) {
-    let current = quizQuestions[q];
+// get variables
+var countdownTimer = document.getElementById('countdown');
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var choice = document.querySelectorAll(".choice");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var choiceD = document.getElementById("D");
+var finalScore = document.getElementById("my-score");
+var saveBtn = document.querySelector('.save-button');
+var result = document.getElementById('result');
 
-question.textContent = current.question;
-    console.log(current.question);
-choiceA.textContent = current.choiceA;
-    console.log(current.choiceA);
-choiceB.textContent = current.choiceB;
-    console.log(current.choiceB);
-choiceC.textContent = current.choiceC;
-    console.log(current.choiceC);
-choiceD.textContent = current.choiceD;
-    console.log(current.choiceD);
-answer = current.answer;
-    console.log(current.answer);
-}
+var q = 0
+var timeLeft = 59;
+var score = 0;
 
-var checkAnswer = function(click) {
-    var selection = (choice = click.target).id
-    console.log(selection);
-    if (selection === answer) {
-        correctAnswer(); 
-        console.log("correct!");
-    } else {
-        // incorrectAnswer()        
+// display questions by index
+function displayNextQuestion() {
+    var current = quizQuestions[q];
+    question.textContent = current.question;
+    choiceA.textContent = current.choiceA;
+    choiceB.textContent = current.choiceB;
+    choiceC.textContent = current.choiceC;
+    choiceD.textContent = current.choiceD;
+    answer = current.answer;
+
+    if (q < quizQuestions.length) {
+        q++;
+        console.log('q = ' + q)
     }
 }
 
+// check if selected answer is correct
+var checkAnswer = function (click) {
+    var selection = (choice = click.target).id
+    console.log('you selected ' + selection);
+    if (selection === answer) {
+        correctAnswer()
+    } else if (selection !== answer) {
+        incorrectAnswer()
+    }
+};
 quiz.addEventListener("click", checkAnswer)
 
+// run if selected answer is correct
 function correctAnswer() {
-    if (q < quiz.length) {
-        q = q++
-        displayNextQuestion(+1);
-        console.log(displ)
+    score++;
+    console.log('score: ' + score)
+    result.textContent = "That is correct!";
+    result.className = 'select-correct';
+    if (q < quizQuestions.length) {
+        displayNextQuestion();
+    } else {
+        finalQuestion();
     }
-        choicesCorrect++;
-        var result = document.createElement("h3");
-        result.textContent = "Correct!";
-        quiz.appendChild(result);
-    };
+};
 
+// run if selected answer is incorrect
+function incorrectAnswer() {
+// decrement 10 s for incorrect answer
+    result.textContent = "Sorry, that is incorrect.";
+    result.className = 'select-incorrect';
+    if (q < quizQuestions.length) {
+        displayNextQuestion();
+    } else {
+        finalQuestion();
+    }
+};
 
+function finalQuestion() {
+    clearInterval(countdownInterval);
+    countdownTimer.textContent = "Congratulations, you have completed the quiz!";
+
+    quiz.removeEventListener('click', displayNextQuestion);
+    
+    var viewScoreBtn = document.createElement('button');
+    viewScoreBtn.textContent = "View Final Score";
+    quiz.appendChild(viewScoreBtn);
+    viewScoreBtn.addEventListener('click', endOfQuiz);
+};
+
+// set countdown timer
+const countdownInterval = setInterval(countdown, 1000)
 function countdown() {
-    // select timer
-    var countdownTimer = document.getElementById('countdown');
-
-    // set interval
-    var timeInterval = setInterval(function () {
-        // decrement timer
-        if (timeLeft >= 1) {
-            countdownTimer.textContent = timeLeft;
-            timeLeft--;
-            // set time's up message and clear timer when timeLeft = 0
-        } else {
-            countdownTimer.textContent = "Time's up!";
-            clearInterval(timeInterval);
-            quiz.hidden = true;
-            quizComplete.hidden = false;
-            // Call the `saveScore()` function
-            // saveScore();
-        }
-    }, 1000);
+    if (timeLeft >= 1) {
+        countdownTimer.textContent = timeLeft;
+        timeLeft--;
+        // set 'time's up' message and clear timer when timeLeft = 0
+    } else {
+        countdownTimer.textContent = "Time's up!";
+        endOfQuiz()
+        return;
+        // Call the `saveScore()` function
+        // saveScore();
+    }
+    // }, 1000);
 }
 
+// end of quiz (out of time or answered final question)
+function endOfQuiz() {
+    quiz.hidden = true;
+    quizComplete.hidden = false;
+    finalScore.textContent = score / choice.length
+}
+
+// start quiz
 function startQuiz() {
     if (startPage.hidden === false && quiz.hidden === true) {
         startPage.hidden = true;
         quiz.hidden = false;
     }
-
+    // start countdown
     countdown();
-    displayNextQuestion(q=0);
+    // display first question
+    displayNextQuestion();
 }
 
+// event listener for start button, call startQuiz()
 startBtn.addEventListener("click", startQuiz);
