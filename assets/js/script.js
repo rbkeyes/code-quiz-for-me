7a1ac7803cc48deb24853f5da065e9e05153acff
Reed Keyes <rbkeyes@gmail.com>
2022-12-02 14:51:46 -0800

    reverted to earlier commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ const startBtn = document.getElementById("start");
 // timer variables
 var countdownTimer = document.getElementById('countdown');
 var timeInterval;
-var timeLeft = 60;
+var timeLeft = 59;
 // remember to change back to 59 before submitting
 
 // quiz variables
@@ -53,17 +53,17 @@ function countdown() {
     if (timeLeft >= 1) {
         countdownTimer.textContent = timeLeft;
         timeLeft--;
-        // set time's up message and clear timer when timeLeft = 0
     } else {
-        allOver();
         question.textContent = "Time's up! Click the button below to view your score."}
-};
+        allOver();
+    };
 
 // end of quiz (out of time or answered final question)
 function allOver() {
     clearInterval(timeInterval);
     countdownTimer.textContent = "0";
     choices.hidden = true;
+    console.log(choices.hidden);
     viewFinalScoreBtn.hidden = false;
     viewFinalScoreBtn.addEventListener('click', endOfQuiz);
 }
@@ -128,12 +128,13 @@ function incorrectAnswer() {
     }
 };
 
-// 
+// display after final question
 function finalQuestion() {
     allOver();
     question.textContent = "Congratulations! You have completed the quiz. Click the button below to view your score."
 };
 
+// calculate score
 function percentCorrect() {
     finalScore = (score/(quizQuestions.length-1))*100;
     console.log(finalScore);
