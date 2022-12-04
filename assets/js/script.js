c48cb60d9c8da072d0639d4137336a8e4e521639
Reed Keyes <rbkeyes@gmail.com>
2022-12-04 14:37:04 -0800

    trying something in a new js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,16 +21,17 @@ var choiceD = document.getElementById("D");
 var result = document.getElementById('result');
 const viewFinalScoreBtn = document.getElementById('view-score-btn');
 
-// quiz complete variables
-const quizComplete = document.getElementById("quiz-complete");
-var score = 0;
-var myScore = document.getElementById("my-score");
-var finalScore;
-var initials = document.getElementById('initials');
-var userScores = [];
-const saveBtn = document.getElementById('save-button');
+// // quiz complete variables
+// const quizComplete = document.getElementById("quiz-complete");
+// var score = 0;
+// var myScore = document.getElementById("my-score");
+// var finalScore;
+// var initials = document.getElementById('initials');
+// var savedInitials = [];
+// var userScores = [];
+// const saveBtn = document.getElementById('save-button');
 
-const scoreboard = document.getElementById('scoreboard');
+// const scoreboard = document.getElementById('scoreboard');
 
 // start quiz
 function startQuiz() {
@@ -145,58 +146,58 @@ function endOfQuiz() {
     quizComplete.hidden=false;
 };
 
-saveBtn.addEventListener('click', saveUserScore);
-function saveUserScore() { 
-    getSavedScores()
-    console.log(userScores);
-    userScores.push(initials.value + ': ' + finalScore);
-    console.log('savedScores');
-    console.log(userScores);
-    localStorage.setItem('saved-scores', (JSON.stringify(userScores)));
-    console.log('saved to local storage: ')
-    console.log(localStorage);
-    // empty userScores variable after saving to storage to avoid array within an array, avoid duplicate
-    userScores;
-    console.log("clear user scores");
-    console.log(userScores);
-    viewScoreboard();
-};
+// saveBtn.addEventListener('click', saveUserScore);
+// function saveUserScore() { 
+//     getSavedScores()
+//     savedInitials.push(initials.value);
+//     console.log('savedScores');
+//     console.log(userScores);
+//     localStorage.setItem("initials", (JSON.stringify(savedInitials)));
+//     localStorage.setItem(initials.value, (JSON.stringify(finalScore)));
+//     console.log('saved to local storage: ')
+//     console.log(localStorage);
+//     savedInitials;
+//     viewScoreboard();
+// };
 
-// "view high scores" variables
-const viewSavedScores = document.getElementById('view-high-scores');
-var backToStart = document.getElementById('to-start-page');
+// // "view high scores" variables
+// const viewSavedScores = document.getElementById('view-high-scores');
+// var backToStart = document.getElementById('to-start-page');
 
-// get saved scored when "view saved scores" is clicked
-viewSavedScores.addEventListener('click', getSavedScores);
-function getSavedScores() {
-    var getScores = JSON.parse(localStorage.getItem('saved-scores'));
-    if (getScores !== null) {
-        console.log(getScores);
-        userScores = getScores;
-        console.log(typeof userScores);
-} 
-viewScoreboard();
-}
+// // get saved scored when "view saved scores" is clicked
+// viewSavedScores.addEventListener('click', getSavedScores);
+// function getSavedScores() {
+//     var getInitials = JSON.parse(localStorage.getItem('initials'));
+//     savedInitials.push(getInitials);
+//     var getScores = JSON.parse(localStorage.getItem(initials.value));
+//     if (getScores !== null) {
+//         console.log(savedInitials);
+//         console.log(getScores);
+//         // userScores.push(getScores);
+//         // console.log(typeof userScores);
+// } 
+// viewScoreboard();
+// }
 
-// render scores to scoreboard
-function viewScoreboard() {
-    renderHighScores();
-    startPage.hidden=true;
-    quiz.hidden=true;
-    quizComplete.hidden=true;
-    scoreboard.hidden=false;
-    for (var s = 0; s < userScores.length; s++){
-        highScore = (document.getElementById(s + 1).textContent = userScores[s] + '%');
-        console.log(highScore);
-    } 
-    }
+// // render scores to scoreboard
+// function viewScoreboard() {
+//     // renderHighScores();
+//     startPage.hidden=true;
+//     quiz.hidden=true;
+//     quizComplete.hidden=true;
+//     scoreboard.hidden=false;
+//     for (var s = 0; s < savedInitials.length; s++){
+//         highScore = (document.getElementById(s + 1).textContent = userScores[s] + '%');
+//         console.log(highScore);
+//     } 
+//     }
 
-function renderHighScores() {
-    var highScores = Object.fromEntries(
-                    Object.entries(userScores).sort( (a,b) => a[1] - b[1] )    
-                 ) 
-    console.log('Sorted object: ', highScores); 
-};
+// function renderHighScores() {
+//     var highScores = Object.fromEntries(
+//                     Object.entries(userScores).sort( (a,b) => a[1] - b[1] )    
+//                  ) 
+//     console.log('Sorted object: ', highScores); 
+// };
 
 backToStart.addEventListener('click', returnToStartPage)
 function returnToStartPage() {
